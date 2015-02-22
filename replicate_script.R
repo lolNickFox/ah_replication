e173305dd579937d5ef0206ff4205e572741faef
lolNickFox <nwf7@scarletmail.rutgers.edu>
2015-02-21 23:53:31 -0500

    clean up workflow

--- replicate_script.R
+++ replicate_script.R
@@ -1,28 +1,7 @@
-wave1 <- read.table("wave_1_data.tsv", header = TRUE, sep = "\t")
-wave3 <- read.table("wave_3_data.tsv", header = TRUE, sep = "\t")
-wave3 <- read.table("wave_3_data.xlsx", header = TRUE, sep = "\t")
+wave1 <- read.table("wave_1_data.tsv", header = TRUE, sep = "\t") 
+#reads in wave 1 data
 
-library(xlsx)
-wave3 <- read.xlsx("wave_3_data.xlsx", sheetName = "Sheet 1")
+wave3 <- read.csv("wave_3_data_spss.csv") 
+#reads in wave 3 data ##had to export the wave 3 spss file as a csv to get R to read this correctly.
 
-wave3 <- read.table("wave_3_data.tsv", header = TRUE, fill = TRUE, sep = "\t")
-
-View(wave3)
-
-library(dplyr)
-
-wave3 %>%
-  arrange(CASEID)
-
-wave3 <- tbl_df(wave3)
-
-wave3 %>%
-  arrange(CASEID)
-
-wave3
-
-tail(wave3)
-
-wave3 <- read.delim("wave_3_data.tsv")
-
-wave3 <- read.csv("wave_3_data_spss.csv")
+wave3_weights <- read.table("wave_3_weights.tsv", header = TRUE, sep = "\t") #read in the wave 3 weights

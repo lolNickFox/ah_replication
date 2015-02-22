fe785fefa6f221304f6acb5e0d55a89769b75454
lolNickFox <nwf7@scarletmail.rutgers.edu>
2015-02-22 00:02:50 -0500

    cleaned up some comments

--- replicate_script.R
+++ replicate_script.R
@@ -2,6 +2,7 @@ wave1 <- read.table("wave_1_data.tsv", header = TRUE, sep = "\t")
 #reads in wave 1 data
 
 wave3 <- read.csv("wave_3_data_spss.csv") 
-#reads in wave 3 data ##had to export the wave 3 spss file as a csv to get R to read this correctly.
+#reads in wave 3 data - had to export the wave 3 spss file as a csv to get R to read this correctly.
 
-wave3_weights <- read.table("wave_3_weights.tsv", header = TRUE, sep = "\t") #read in the wave 3 weights
+wave3_weights <- read.table("wave_3_weights.tsv", header = TRUE, sep = "\t") 
+#read in the wave 3 weights
